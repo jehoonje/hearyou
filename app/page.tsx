cd3a131bcf3fe01499e78836de97fe6ff8fe7fcd
Jehoon <limjhoon8@gmail.com>
2025-04-21 00:35:22 +0900

    💄 Style: 로그인 폼 안내 메세지처리

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import { cookies } from 'next/headers';
 import ClientPage from './client-page';
 import { fetchKeywords } from '../utils/fetchKeywords'; // 경로 확인 필요
 import { Keyword } from '../types'; // Keyword 타입 임포트 (필요시)
-import { supabase } from '../lib/supabase'; // Database 타입 임포트 (필요시)
+import { Database } from '../types/supabase'; // Database 타입 임포트 (필요시)
 
 // 서버 컴포넌트로 초기 데이터 로딩
 export default async function Home() {
   // 서버 컴포넌트에서 Supabase 클라이언트 생성
-  const supabase = createServerComponentClient<supabase>({ cookies }); // 타입 지정
+  const supabase = createServerComponentClient<Database>({ cookies }); // 타입 지정
 
   // 사용자 세션 확인
   const { data: { session }, error } = await supabase.auth.getSession();
